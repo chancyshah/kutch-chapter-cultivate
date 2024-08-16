73e0eb7f08a29a3ca65380f633062ec66003c650
Chancy Shah <72352809+chancyshah@users.noreply.github.com>
2024-08-16 13:16:23 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import subprocess
 import sys
 
 def run_streamlit():
-    subprocess.run([sys.executable, "-m", "uvicorn", "api.main:app", "--host", "0.0.0.0", "--port", "8002"])
+    subprocess.run([sys.executable, "-m", "streamlit", "run", "app/main.py"])
 
 def run_fastapi():
-    subprocess.run([sys.executable, "-m", "streamlit", "run", "app/main.py", "--server.port", "8504", "--server.runOnSave", "false"])
+    subprocess.run([sys.executable, "-m", "uvicorn", "api.main:app", "--reload"])
 
 if __name__ == "__main__":
     import threading
